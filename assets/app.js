0d048ef49a11aa49ae867333c69ad927eea2ab4d
Maxime Ferraille <maxime.ferraille@gmail.com>
2021-03-03 08:27:10 +0100

    Correction Q1 -> Q7

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,18 @@
 "use strict"
 
 $(document).ready(function() {
-  console.log("Code goes here");
+  // Q4 Burger menu appears or disappears when the corresponding button is clicked
+  // Bind click on nav button
+  $(".navbar-toggler").on("click", function() {
+    $("#navbarSupportedContent").toggle();
+  });
+
+  // Q5 Bind click on each nav link
+  $(".nav-link").on("click", function(event) {
+    // Q6 Prevent browser default behavior
+    event.preventDefault();
+
+    // Q7 Scroll instantly to this position
+    $("html, body").animate({ scrollTop: 500 }, 1000);
+  });
 });
